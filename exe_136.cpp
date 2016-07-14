767ca818684806a329b5c5a750cadd4f07b9ff1d
Duc Do <dotrananhduc@gmail.com>
2016-07-14 11:00:42 +0700

    exe_136

---
+++ exe_136.cpp
@@ -0,0 +1,28 @@
+﻿/*
+	Exe_136: Tìm số chẵn cuối cùng trong mảng một chiều các số thực. Nếu không có trả về -1
+*/
+#include <iostream>
+
+using namespace std;
+
+int Result(int a[], int n)
+{
+	for(int i = n - 1; i >= 0; i--)
+	{
+		if(a[i] % 2 == 0)
+		{
+			return a[i];
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
