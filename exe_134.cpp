0621241484fef43fad4de2c80959f74303d875f1
Duc Do <dotrananhduc@gmail.com>
2016-07-14 10:55:03 +0700

    exe_134

---
+++ exe_134.cpp
@@ -0,0 +1,29 @@
+﻿/*
+	Exe_134: Viết hàm tìm giá trị lớn nhất trong mảng 1 chiều các số thực
+*/
+#include <iostream>
+
+using namespace std;
+
+double Result(double a[], int n)
+{
+	double max = a[0];
+	for(int i = 1; i < n; i++)
+	{
+		if(a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	double a[100];
+	int n;	
+	double kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
