356c8cb2891b57b999b343d15bfaa03b8468226a
Duc Do <dotrananhduc@gmail.com>
2016-07-14 10:46:47 +0700

    exe_130

---
+++ exe_130.cpp
@@ -0,0 +1,21 @@
+﻿/*
+	Viết hàm xuất mảng một chiều các số thực
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	float a[100];
+	cout << "Input n: ";
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i];
+	}
+
+	return 0;
+}
