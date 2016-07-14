6c0f0af1edd22c0a25268380670e6cbddb2ebb8c
Duc Do <dotrananhduc@gmail.com>
2016-07-14 10:47:25 +0700

    exe_131

---
+++ exe_131.cpp
@@ -0,0 +1,21 @@
+﻿/*
+	Viết hàm xuất mảng một chiều các số nguyên
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int a[100];
+	cout << "Input n: ";
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i];
+	}
+
+	return 0;
+}
