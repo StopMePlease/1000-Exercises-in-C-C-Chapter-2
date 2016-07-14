bcae9171634ba3a02f492dd684b07a9d6f3e9304
Duc Do <dotrananhduc@gmail.com>
2016-07-14 11:03:55 +0700

    exe_137

---
+++ exe_137.cpp
@@ -0,0 +1,29 @@
+﻿/*
+	Exe_137: Tim "một vị trí mà giá trị tại vị trí đó là giá trị nhỏ nhất" trong mảng số thực
+*/
+#include <iostream>
+
+using namespace std;
+
+int Result(double a[], int n)
+{
+	int min = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] < a[min])
+		{
+			min = i;
+		}
+	}
+	return min;
+}
+
+int main()
+{
+	double a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
