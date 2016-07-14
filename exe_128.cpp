034ca2ad9b657d0cae738ca0f927c21347163359
Duc Do <dotrananhduc@gmail.com>
2016-07-14 10:43:43 +0700

    exe_128

---
+++ exe_128.cpp
@@ -0,0 +1,21 @@
+﻿/*
+	Viết hàm nhập mảng một chiều các số thực
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, a[100];
+	cout << "Input n: ";
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+
+	return 0;
+}
