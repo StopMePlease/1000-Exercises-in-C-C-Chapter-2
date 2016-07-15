17afac20382bf8c90c6f2c9398483050e040944e
Duc Do <dotrananhduc@gmail.com>
2016-07-15 20:55:38 +0700

    exe_139

---
+++ exe_139.cpp
@@ -0,0 +1,41 @@
+﻿/*
+	Exe_139: Tìm "vị trí số hoàn thiện cuối cùng" trong mảng 1 chiều các số nguyên. Nếu không có trả về -1
+*/
+#include <iostream>
+
+using namespace std;
+
+bool Check(int n)
+{
+	int sum = 0;
+	for(int i = 1; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			sum += i;
+		}
+	}
+	return sum == n ? true : false;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = n; i >= 0; i--)
+	{
+		if(Check(a[i]))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
