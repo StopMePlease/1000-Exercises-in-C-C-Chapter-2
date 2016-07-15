8db88d55e0dbb128a8d5d6fb6dd1f2994297daf6
Duc Do <dotrananhduc@gmail.com>
2016-07-15 21:13:31 +0700

    exe_143

---
+++ exe_143.cpp
@@ -0,0 +1,28 @@
+﻿/*
+	Exe_143: Tìm "vị trí số chẵn đầu tiên" trong mảng 1 chiều các số nguyên. Nếu không có trả về -1
+*/
+#include <iostream>
+
+using namespace std;
+
+int Result(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		if(a[i] % 2 == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
