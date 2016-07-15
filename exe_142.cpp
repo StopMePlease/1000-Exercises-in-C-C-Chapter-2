0502a9c1f36e976d816b60acbdabd342ba9e4131
Duc Do <dotrananhduc@gmail.com>
2016-07-15 21:11:15 +0700

    exe_142

---
+++ exe_142.cpp
@@ -0,0 +1,29 @@
+﻿/*
+	Exe_142: Tìm "giá trị nhỏ nhất" trong mảng một chiều số thực
+*/
+#include <iostream>
+
+using namespace std;
+
+double Result(double a[], int n)
+{
+	double min = a[0];
+	for(int i = 1; i < n; i++)
+	{
+		if(a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	return min;
+}
+
+int main()
+{
+	double a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
