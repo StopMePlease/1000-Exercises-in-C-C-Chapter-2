0f1538e2c6b4b3b70f5b40f5555edcb3f27fd025
Duc Do <dotrananhduc@gmail.com>
2016-07-16 15:09:15 +0700

    exe_151

---
+++ exe_151.cpp
@@ -0,0 +1,57 @@
+﻿/*
+	Exe_151: Tìm "số nguyên tố lớn nhất" trong mảng 1 chiều các số nguyên. Nếu không có trả về 0
+*/
+#include <iostream>
+
+using namespace std;
+
+bool Check(int n)
+{
+	if(n == 2)
+	{
+		return true;
+	}
+
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int Result(int a[], int n)
+{
+	int vt = -1;
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(Check(a[i]))
+		{
+			sum = a[i];
+			vt = i;
+			break;
+		}
+	}
+
+	for(int i = vt + 1; i < n; i++)
+	{
+		if(Check(a[i]) && a[i] > sum)
+		{
+			sum = a[i];
+		}
+	}
+	return sum;
+}
+
+int main()
+{
+	int a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
