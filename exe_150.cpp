8f491de54fb092af2d02041eab68dde2475bef55
Duc Do <dotrananhduc@gmail.com>
2016-07-16 15:00:15 +0700

    exe_150

---
+++ exe_150.cpp
@@ -0,0 +1,40 @@
+﻿/*
+	Exe_150: Tìm "giá trị âm lớn nhất" trong mảng 1 chiều các số thực. Nếu không có trả về 0
+*/
+#include <iostream>
+
+using namespace std;
+
+double Result(double a[], int n)
+{
+	int vt = -1;
+	double min = 0;
+	for(int i = 0; i < n ; i++)
+	{
+		if(a[i] < 0)
+		{
+			vt = i;
+			min = a[i];
+			break;
+		}
+	}
+
+	for(int i = vt; i < n; i++)
+	{
+		if(a[i] < 0 && a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	return min;
+}
+
+int main()
+{
+	double a[100];
+	int n;	
+	double kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
