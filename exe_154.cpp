18479cc71ff64c139695ff2781d8c02f5a6aae84
Duc Do <dotrananhduc@gmail.com>
2016-07-16 15:02:19 +0700

    exe_154

---
+++ exe_154.cpp
@@ -0,0 +1,40 @@
+﻿/*
+	Exe_154: Tìm "vị trí giá trị âm lớn nhất" trong mảng 1 chiều các số thực. Nếu không có trả về -1
+*/
+#include <iostream>
+
+using namespace std;
+
+double Result(double a[], int n)
+{
+	int vt = -1;
+	double min = 0;
+	for(int i = 0; i < n ; i++)
+	{
+		if(a[i] < 0)
+		{
+			vt = i;
+			min = a[i];
+			break;
+		}
+	}
+
+	for(int i = vt + 1; i < n; i++)
+	{
+		if(a[i] < 0 && a[i] < min)
+		{
+			vt = i;
+		}
+	}
+	return vt;
+}
+
+int main()
+{
+	double a[100];
+	int n;	
+	int kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
