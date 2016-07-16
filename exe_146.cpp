449fd4dc77242234cccd6b3de026b6006db26fc7
Duc Do <dotrananhduc@gmail.com>
2016-07-16 14:46:47 +0700

    exe_146

---
+++ exe_146.cpp
@@ -0,0 +1,27 @@
+﻿/*
+	Exe_146: Tìm "giá trị âm đầu tiên" trong mảng 1 chiều các số thực. Nếu không có trả về 1
+*/
+#include <iostream>
+
+using namespace std;
+
+double Result(double a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] < 0)
+		{
+			return a[i];
+		}
+	}
+	return 1;
+}
+int main()
+{
+	double a[100];
+	int n;	
+	double kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
