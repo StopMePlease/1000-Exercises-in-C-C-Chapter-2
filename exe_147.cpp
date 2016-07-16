5428da6a7df7c43b350a47b699716d2b95342fea
Duc Do <dotrananhduc@gmail.com>
2016-07-16 14:49:31 +0700

    exe_147

---
+++ exe_147.cpp
@@ -0,0 +1,27 @@
+﻿/*
+	Exe_147: Tìm "số dương cuối cùng" trong mảng 1 chiều các số nguyên. Nếu không có trả về -1
+*/
+#include <iostream>
+
+using namespace std;
+
+int Result(int a[], int n)
+{
+	for(int i = n - 1; i > 0; i--)
+	{
+		if(a[i] > 0)
+		{
+			return a[i];
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int a[100];
+	int n;	
+	double kq = Result(a, n);
+	cout << kq;
+
+	return 0;
+}
