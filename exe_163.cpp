29b77831857bebf2856845df35c12ca49df25029
Duc Do <dotrananhduc@gmail.com>
2016-07-18 09:29:44 +0700

    exe_163

---
+++ exe_163.cpp
@@ -0,0 +1,55 @@
+﻿/*
+	Exe_163: Tìm số chính phương đầu tiên trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	return n == pow(sqrt((double)n), 2);
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		if(Check(a[i]))
+		{
+			return a[i];
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
