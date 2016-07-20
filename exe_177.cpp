d2f3a62444e3079a20cf85b3227e9d1602582193
Duc Do <dotrananhduc@gmail.com>
2016-07-20 16:23:39 +0700

    exe_177

---
+++ exe_177.cpp
@@ -0,0 +1,53 @@
+﻿/*
+	Exe_177: Hãy liệt kê các giá trị trong mảng 1 chiều các số thực thuộc đoạn [x,y] cho trước
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n,int x,int y)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] <= y && a[i] >= x)
+		{
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n, x, y;
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input x: ";
+	cin >> x;
+	cout << "Input y: ";
+	cin >> y;
+	Input(a, n);
+	Output(a, n);
+	Result(a, n, x, y);
+	
+	return 0;
+}
