f39635626bb7ca22d21229d15fbf226cb552e247
Duc Do <dotrananhduc@gmail.com>
2016-07-20 16:54:27 +0700

    exe_190

---
+++ exe_190.cpp
@@ -0,0 +1,64 @@
+﻿/*
+	Exe_190: Hãy liệt kê các giá trị có toàn chữ số lẻ trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	if(n % 2 == 0)
+		return false;
+
+	while(n)
+	{
+		if(n % 2 == 0)
+			return false;
+		n /= 10;
+	}
+	return true;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(Check(a[i]))
+		{
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
