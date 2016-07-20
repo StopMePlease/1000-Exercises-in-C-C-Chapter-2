8dd4c4c6148a44732f638134aeb9a41e0a1a2fe5
Duc Do <dotrananhduc@gmail.com>
2016-07-20 16:27:13 +0700

    exe_179

---
+++ exe_179.cpp
@@ -0,0 +1,50 @@
+﻿/*
+	Exe_179: Hãy liệt kê các giá trị trong mảng mà thỏa điều kiện lớn hơn trị tuyệt đối của giá trị liền sau nó
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] > abs(a[i + 1]))
+		{
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
