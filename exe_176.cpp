b73c0f09960ed0352ac466a663a422c3fdf280cd
Duc Do <dotrananhduc@gmail.com>
2016-07-20 16:19:32 +0700

    exe_176

---
+++ exe_176.cpp
@@ -0,0 +1,49 @@
+﻿/*
+	Exe_176: Hãy liệt kê các số âm trong mảng 1 chiều các số thực
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] < 0)
+		{
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
