8ef21c5c4a3c4e0e222d69338e2ddaa676adade6
Duc Do <dotrananhduc@gmail.com>
2016-07-20 15:25:51 +0700

    exe_174

---
+++ exe_174.cpp
@@ -0,0 +1,52 @@
+﻿/*
+	Exe_174: Cho mảng số thực nhiều hơn 2 vị trí và các giá trị trong mảng khác nhau từng đôi một. Hãy viết hàm liệt kê tất cả các cặp giá trị (a,b) trong mảng thỏa điều kiện a<= b
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i] <= a[j] || a[j] >= a[i])
+			{
+				cout << a[i] << ", " << a[j] << endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
