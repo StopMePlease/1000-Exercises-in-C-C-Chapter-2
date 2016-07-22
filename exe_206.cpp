f3ed90afaa581d6287fe3577a053f38bb698d323
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:15:45 +0700

    exe_206

---
+++ exe_206.cpp
@@ -0,0 +1,51 @@
+﻿/*
+	Exe_206: Tính tổng các giá trị lớn hơn các giá trị xung quanh trong máng chiều các số thực
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	int sum = 0;
+	for(int i = 1; i < n - 1; i++)
+	{
+		if(a[i] > abs(a[i + 1]) && a[i] > a[i - 1])
+			sum += a[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
