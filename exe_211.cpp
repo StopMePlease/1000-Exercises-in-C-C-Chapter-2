75bde6c2c360af5b2148c22da4a944a472148a13
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:29:07 +0700

    exe_211

---
+++ exe_211.cpp
@@ -0,0 +1,72 @@
+﻿/*
+	Exe_211: Tính trung bình cộng các số nguyên tố trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	if(n == 2)
+	{
+		return true;
+	}
+
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int Result(int a[], int n)
+{
+	int sum = 0;
+	int dem = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(Check(a[i]))
+		{
+			sum += a[i];
+			dem++;
+		}
+	}
+	return sum / dem;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
