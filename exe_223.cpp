0d39fe834b02000700eb11e6eec2193eeb2efe9b
Duc Do <dotrananhduc@gmail.com>
2016-07-22 21:21:10 +0700

    exe_223

---
+++ exe_223.cpp
@@ -0,0 +1,70 @@
+﻿/*
+	Exe_223: Đếm số lượng số nguyên tố trong mảng nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	if(n == 2)
+	{
+		return true;
+	}
+
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int Result(int a[], int n)
+{
+	int dem = 0;
+	for(int i = 1; i < n - 1; i++)
+	{
+		if(Check(a[i]))
+		{
+			dem++;
+		}
+	}
+	return dem;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
