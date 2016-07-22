af53e203c42f78cad05781cefcfaa1d2a75cfb14
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:36:45 +0700

    exe_216

---
+++ exe_216.cpp
@@ -0,0 +1,53 @@
+﻿/*
+	Exe_216: Đếm số lượng chữ số chẵn trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	int dem = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] > 0)
+		{
+			dem += 1;;
+		}
+	}
+	return dem;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
