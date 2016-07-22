f370031b86417873492d241179b9e0eb3d7586cc
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:19:40 +0700

    exe_208

---
+++ exe_208.cpp
@@ -0,0 +1,56 @@
+﻿/*
+	Exe_208: Tính tổng các giá trị chính phương trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	return n == pow(sqrt((double)n), 2);
+}
+
+int Result(int a[], int n)
+{
+	int sum = 0;
+	for(int i = 1; i < n - 1; i++)
+	{
+		if(Check(a[i]))
+			sum += a[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
