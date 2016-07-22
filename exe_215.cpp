0ffd3bda4cf70df098d6c67cf929d701b723861c
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:35:37 +0700

    exe_215

---
+++ exe_215.cpp
@@ -0,0 +1,55 @@
+﻿/*
+	Exe_215: Tính khoảng cách trung bình giữa các giá trị trong mảng
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	int sum = 1;
+	int dem = 0;
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			sum += abs(a[i] - a[i]);
+			dem++;
+		}
+	}
+	return sum / dem;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
