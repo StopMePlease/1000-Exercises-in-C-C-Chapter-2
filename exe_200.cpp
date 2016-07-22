7fd661fa8786bd9c5b846b9bc8fe79223c153612
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:02:54 +0700

    exe_200

---
+++ exe_200.cpp
@@ -0,0 +1,50 @@
+﻿/*
+	Exe_200: Tính tổng các phần tử trong mảng
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+	{
+		sum += a[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
