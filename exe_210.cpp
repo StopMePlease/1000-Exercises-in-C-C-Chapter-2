4527b32d3e3de1f924fefcac9b6d13e29d22ddfd
Duc Do <dotrananhduc@gmail.com>
2016-07-22 17:27:26 +0700

    exe_210

---
+++ exe_210.cpp
@@ -0,0 +1,67 @@
+﻿/*
+	Exe_210: Tính tổng các giá trị có chữ số đầu tiên là chữ số chẵn có trong mảng các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	while(n)
+	{
+		n /= 10;
+		if(n < 10 && n % 2 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}	
+}
+
+int Result(int a[], int n)
+{
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(Check(a[i]))
+			sum += a[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
