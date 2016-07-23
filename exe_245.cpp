f95fc38b9759ed4d622009b3247df9a03e46fd23
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:32:03 +0700

    exe_245

---
+++ exe_245.cpp
@@ -0,0 +1,52 @@
+﻿/*
+	Exe_245: Kiểm tra mảng nguyên có tồn tại toàn số chẵn. Không tồn tại -> 0, có tồn tại -> 1
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] % 2 != 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;	
+	
+	return 0;
+}
