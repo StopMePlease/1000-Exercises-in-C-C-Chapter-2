9ebeb15a0e60e42977334a31ba4077e324a1bfd7
Duc Do <dotrananhduc@gmail.com>
2016-07-23 14:51:45 +0700

    exe_230

---
+++ exe_230.cpp
@@ -0,0 +1,72 @@
+﻿/*
+	Exe_230: Hãy liệt kê tần suất xuất hiện của các giá trị trong mảng.
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int check;
+	int dem;
+	for(int i = 0; i < n; i++)
+	{
+		check = 0;
+		dem = 1;
+		for(int j = i - 1; j >= 0; j--)
+		{
+			if(a[j] == a[i])
+			{
+				check = 1;
+				break;
+			}
+		}
+		if(check == 1)
+		{
+			continue;
+		}
+
+		for(int k = i + 1; k < n; k++)
+		{
+			if(a[k] == a[i])
+			{
+				dem++;
+			}
+		}
+
+		cout << a[i] << ": " << dem << endl;
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
