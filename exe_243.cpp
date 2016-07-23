b45bc0de9156c58a7cb7ce99da819717a8b6d8d7
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:29:41 +0700

    exe_243

---
+++ exe_243.cpp
@@ -0,0 +1,69 @@
+﻿/*
+	Exe_243: Kiểm tra mảng nguyên có tồn tại số nguyên tố. Không tồn tại -> 0, có tồn tại -> 1
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	if(n == 2)
+	{
+		return true;
+	}
+
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 0; i < n - 1; i++)
+	{
+		if(Check(a[i]))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;	
+	
+	return 0;
+}
