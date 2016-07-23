4478f154e7127b3b3abd9f915a2fcc3bc34583a2
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:50:49 +0700

    exe_254

---
+++ exe_254.cpp
@@ -0,0 +1,53 @@
+﻿/*
+	Exe_254: Đếm số lượng giá trị trong mảng thỏa tính chất "lớn hơn tất cả các giá trị đứng đằng trước nó"
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	int dem = 0;
+	for(int i = 1; i < n; i++)
+	{
+		if(a[i] > abs(a[i - 1]))
+		{
+			dem++;
+		}
+	}
+	return dem;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;
+	
+	return 0;
+}
