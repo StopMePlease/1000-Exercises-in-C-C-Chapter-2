1081950968a072d83f08d884385d1b928395bc9e
Duc Do <dotrananhduc@gmail.com>
2016-07-24 06:06:34 +0700

    exe_262

---
+++ exe_262.cpp
@@ -0,0 +1,81 @@
+﻿/*
+	Exe_262: Sắp xếp các số chẵn trong mảng tăng dần, số lẻ tăng dần. Vị trí tương đối giữa các số chẵn lẻ không đổi
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void Result(int a[], int n)
+{
+	int check = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] % 2 == 0)
+		{
+			for(int j = i; j < n; j++)
+			{
+				if(a[j] % 2 == 0)
+				{
+					if(a[i] > a[j])
+					{
+						Swap(a[i], a[j]);
+					}
+				}
+			}
+		}
+		else
+		{
+			for(int j = i; j < n; j++)
+			{
+				if(a[j] % 2 != 0)
+				{
+					if(a[i] > a[j])
+					{
+						Swap(a[i], a[j]);
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
