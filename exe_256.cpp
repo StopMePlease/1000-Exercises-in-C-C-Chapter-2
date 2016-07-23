90d00706f48110fe045ae443a1845feae17ede80
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:56:04 +0700

    exe_256

---
+++ exe_256.cpp
@@ -0,0 +1,61 @@
+﻿/*
+	Exe_256: Sắp xếp mảng giảm
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = i; j < n; j++)
+		{
+			if(a[i] < a[j])
+			{
+				Swap(a[i], a[j]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
