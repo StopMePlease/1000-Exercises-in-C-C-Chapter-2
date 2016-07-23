1d8f7186742bfafeb82ebc3be29162ffd711230d
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:35:47 +0700

    exe_246

---
+++ exe_246.cpp
@@ -0,0 +1,52 @@
+﻿/*
+	Exe_246: Kiểm tra mảng thực có tồn tại đối xứng . Không tồn tại -> 0, có tồn tại -> 1
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 0; i < n / 2; i++)
+	{
+		if(a[i] != a[n - i -1])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;	
+	
+	return 0;
+}
