c1891c062b08809fe4ffbfe2951378321c0da3a4
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:38:55 +0700

    exe_249

---
+++ exe_249.cpp
@@ -0,0 +1,52 @@
+﻿/*
+	Exe_249: Kiểm tra mảng có giảm dần hay không?
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 0; i < n - 1; i++)
+	{
+		if(a[i] < a[i + 1])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;	
+	
+	return 0;
+}
