3377fc768ef8118f0e1f5c2d5bbd526cb2d3ca09
Duc Do <dotrananhduc@gmail.com>
2016-07-23 15:26:23 +0700

    exe_240

---
+++ exe_240.cpp
@@ -0,0 +1,52 @@
+﻿/*
+	Exe_240: Kiểm tra mảng nguyên có tồn tại giá trị 0 hay không? Không tồn tại -> 0, có tồn tại -> 1
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+int Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] == 0)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	int kq = Result(a, n);
+	cout << kq;	
+	
+	return 0;
+}
