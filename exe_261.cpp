d33233df266d6714e42b1108a72aa5e97ef0eb7a
Duc Do <dotrananhduc@gmail.com>
2016-07-24 05:57:23 +0700

    exe_261

---
+++ exe_261.cpp
@@ -0,0 +1,68 @@
+﻿/*
+	Exe_261: Sắp xếp các số dương trong mảng cac1so61 thực tăng dần. Các vị trí còn lại giữ nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void Result(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] <= 0)
+		{
+			continue;
+		}
+		for(int j = i; j < n; j++)
+		{
+			if(a[j] > 0)
+			{
+				if(a[i] > a[j])
+				{
+					Swap(a[i], a[j]);
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
