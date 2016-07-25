d23402c707a3f7329a540d50f8c98d031d0389a3
Duc Do <dotrananhduc@gmail.com>
2016-07-25 11:20:18 +0700

    exe_277

---
+++ exe_277.cpp
@@ -0,0 +1,72 @@
+﻿/*
+	Exe_277: Xóa tất cả các số nguyên tố trong mảng nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+bool Check(int n)
+{
+	if(n == 2)
+	{
+		return true;
+	}
+
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void Result(int a[], int &n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(Check(a[i]))
+		{
+			for(int j = i; j < n; j++)
+			{
+				a[j] = a[j + 1];
+			}
+			n--;
+			i--;
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n = 0;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
