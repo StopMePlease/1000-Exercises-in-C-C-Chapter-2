82851254e3c35b0316efbfaca671990fba7ba51c
Duc Do <dotrananhduc@gmail.com>
2016-07-25 11:16:27 +0700

    exe_274

---
+++ exe_274.cpp
@@ -0,0 +1,55 @@
+﻿/*
+	Exe_274: Xóa tất cả các số chẵn trong mảng thực
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int &n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] % 2 == 0)
+		{
+			for(int j = i; j < n; j++)
+			{
+				a[j] = a[j + 1];
+			}
+			n--;
+			i--;
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n = 0;
+	cout << "Input n: ";
+	cin >> n;
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
