55a24ee49b3faa8a2c5ac1aa982ca7fc092b4e30
Duc Do <dotrananhduc@gmail.com>
2016-07-26 18:06:42 +0700

    exe_278

---
+++ exe_278.cpp
@@ -0,0 +1,60 @@
+﻿/*
+	Exe_278: Xóa tất cả các phần tử trùng nhau trong mảng, chỉ giữa lại 1 phần tử duy nhất
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int &n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		
+		for(int j = i + 1; j < n; j++)
+		{
+			if(a[j] == a[i])
+			{
+				for(int k = j; k < n; k++)
+				{
+					a[k] = a[k + 1];
+				}
+				n--;
+				j--;
+			}
+		}		
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
