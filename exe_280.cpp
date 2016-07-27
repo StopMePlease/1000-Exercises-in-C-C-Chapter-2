ba5793164bacad565657a2b071a6c6f4b1b6159e
Duc Do <dotrananhduc@gmail.com>
2016-07-27 22:39:19 +0700

    exe_280

---
+++ exe_280.cpp
@@ -0,0 +1,56 @@
+﻿/*
+	Exe_280: Đưa 1 số về đầu mảng
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int temp;
+	cout << "Number selected: ";
+	cin >> temp;
+
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] == temp)
+		{
+			swap(a[0], a[i]);
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
