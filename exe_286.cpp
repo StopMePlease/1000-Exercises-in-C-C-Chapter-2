8c633e439cbf3f4d5c3db7ea7e1596492ac729dd
Duc Do <dotrananhduc@gmail.com>
2016-07-28 20:58:49 +0700

    exe_286

---
+++ exe_286.cpp
@@ -0,0 +1,50 @@
+﻿/*
+	Exe_286: Hãy "dịch trái xoay vòng" các phần tử trong mảng
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int temp = a[0];
+	for(int i = 0; i < n; i++)
+	{		
+		a[i] = a[i + 1];
+	}
+	a[n - 1] = temp;
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
