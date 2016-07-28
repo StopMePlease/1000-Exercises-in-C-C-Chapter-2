2d0c28198dc97940b53ecffb7b55481824d1576a
Duc Do <dotrananhduc@gmail.com>
2016-07-28 20:21:00 +0700

    exe_283

---
+++ exe_283.cpp
@@ -0,0 +1,49 @@
+﻿/*
+	Exe_283: Đảo ngược mảng ban đầu
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int mid = n / 2;
+	for(int i = 0; i < mid; i++)
+	{		
+		swap(a[i], a[n - i - 1]);
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
