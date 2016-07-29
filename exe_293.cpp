6d8b8b6a47653455c95bef7ad66ee17581aa70c5
Duc Do <dotrananhduc@gmail.com>
2016-07-29 14:10:26 +0700

    exe_293

---
+++ exe_293.cpp
@@ -0,0 +1,61 @@
+﻿/*
+	Exe_293: Liệt kê tất cả các mảng con trong mảng 1 chiều các số nguyên
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	cout << "Cac mang con";
+	int num = 0;
+	while(num < n)
+	{
+		cout << a[num] << endl;
+		int temp = num + 1;
+		while(temp < n)
+		{			
+			for(int i = num; i < temp; i++)
+			{
+				cout << a[i] << "   ";
+			}
+			cout << endl;
+			temp++;
+		}
+		num++;
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	Output(a, n);
+
+	return 0;
+}
