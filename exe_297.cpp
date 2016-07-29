bc36576fceac3b6fccfd919ce70e45f00b440ad4
Duc Do <dotrananhduc@gmail.com>
2016-07-29 14:57:11 +0700

    exe_297

---
+++ exe_297.cpp
@@ -0,0 +1,82 @@
+﻿/*
+	Exe_297: Tính tổng từng mảng con tăng trong mảng 1 chiều các số thực
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	cout << "Cac mang con" << endl;
+	int temp;
+	int b[100];
+	int k;
+	for(int i = 0; i < n; i++)
+	{
+		k = 0;
+		temp = a[i];
+		if(a[i + 1] > a[i])
+		{
+			b[k] = a[i];
+			k++;
+		}
+		else
+		{
+			continue;
+		}
+		for(int j = i + 1; j < n; j++)
+		{
+			if(a[j] > temp)
+			{
+				b[k] = a[j];
+				temp = a[j];
+				k++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		int sum = 0;
+		Output(b, k);
+		for(int m = 0; m < k; m++)
+		{
+			sum += b[m];			
+		}
+		cout << sum;
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);	
+
+	return 0;
+}
