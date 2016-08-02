2b223cf22f744f2786b23e72431afce16b7a87a4
Duc Do <dotrananhduc@gmail.com>
2016-08-02 09:32:59 +0700

    exe_303

---
+++ exe_303.cpp
@@ -0,0 +1,98 @@
+﻿/*
+	Exe_303: Tìm dãy con dương dài nhất trong mảng thực
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int temp, temp2 = 0;
+	int start, finish;
+	for(int i = 0; i < n; i++)
+	{
+		temp = 0;	
+		start = i;
+		finish = i;
+		if(a[i] > 0 && a[i + 1] > 0)
+		{
+			temp++;
+			start = i;
+			for(int j = i + 1; j < n; j++)
+			{				
+				if(a[j] <= 0)
+				{
+					finish = j - 1;					
+					break;
+				}
+				temp++;				
+			}			
+		}
+		if(temp > temp2)
+		{
+			temp2 = temp;
+		}
+	}
+
+	cout << "Results: ";
+
+	for(int i = 0; i < n; i++)
+	{
+		temp = 0;		
+		if(a[i] > 0 && a[i + 1] > 0)
+		{
+			temp++;
+			start = i;
+			for(int j = i + 1; j < n; j++)
+			{				
+				if(a[j] <= 0)
+				{
+					finish = j - 1;					
+					break;
+				}
+				temp++;				
+			}			
+		}
+		if(temp == temp2)
+		{
+			for(int k = start; k <= finish; k++)
+			{
+				cout << a[k] << "   "; 
+			}
+			cout << endl;	
+		}		
+	}
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);	
+	
+	return 0;
+}
