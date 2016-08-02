e11469781a384d574f76517d87715efb1a022474
Duc Do <dotrananhduc@gmail.com>
2016-08-02 10:10:54 +0700

    exe_308

---
+++ exe_308.cpp
@@ -0,0 +1,54 @@
+﻿/*
+	Exe_308: Cho mảng thực a. Hãy tạo mảng b, sao cho b chứa các giá trị âm
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void Input(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Output(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[i] << "      ";
+	}
+	cout << endl;
+}
+
+void Result(int a[], int n)
+{
+	int b[100];
+	int k = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i] < 0)
+		{
+			b[k] = a[i];
+			k++;
+		}
+	}
+	Output(b, k);
+}
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);	
+	
+	return 0;
+}
